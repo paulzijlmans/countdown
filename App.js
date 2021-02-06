06662611ae09139883eec1914c1fa1490db92c8b
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-02-06 15:22:49 +0100

    added eventlist

--- App.js
+++ App.js
@@ -1,22 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import EventList from './EventList';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hi there!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <EventList />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ EventList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class EventList extends Component {
+  render() {
+    return (
+      <Text>Hello!</Text>
+    )
+  };
+}
+
+export default EventList;
